010c6aacd16edfe49a6ba9c1cddd03468b694bef
Michel Heide <michel.heide@explido.de>
2012-10-08 16:06:33 +0200

    test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,11 +2,14 @@
 
 function loader($class)
 {
-    $file = 'tests/' .$class . '.php';
+    $file = './tests/' .$class . '.php';
 	echo $file . "\n";
 	
 	var_dump (file_exists($file));
 	echo "\n";
+	echo "--------------------------\n";
+	print_r ($_SERVER);
+	echo "--------------------------\n\n";
     if (file_exists($file)) {
 		echo "require $file \n";
         require $file;
