7f19b6416400d6ebe9d37984e08f4ddb01c64c4a
Michel Heide <michel.heide@explido.de>
2012-10-08 16:40:46 +0200

    test again

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,35 +5,6 @@ function loader($class)
     $file = $class . '.php';
 	echo $file . "\n";
 	
-	var_dump (file_exists($file));
-	echo "\n";
-	var_dump (file_exists('/home/travis/builds/garkeinplan/testproject/tests/' . $file));
-	echo "\n";
-	echo "--------------------------\n";
-	echo getcwd () . "\n\n";
-	chdir('tests');
-	echo getcwd () . "\n\n";
-	
-	if ($handle = opendir(getcwd())) {
-    echo "Directory handle: $handle\n";
-    echo "Files:\n";
-
-    /* Das ist der korrekte Weg, ein Verzeichnis zu durchlaufen. */
-    while (false !== ($file = readdir($handle))) {
-        echo "$file\n";
-    }
-
-    /* Dies ist der FALSCHE Weg, ein Verzeichnis zu durchlaufen. */
-    while ($file = readdir($handle)) {
-        echo "$file\n";
-    }
-
-    closedir($handle);
-}
-	
-	
-	echo "--------------------------\n\n";
-	echo "\n";
     if (file_exists($file)) {
 		echo "require $file \n";
         require $file;
